a888fa6af0b970c944a7c1a35f8de3b732b3c1b3
sdsarun <sdsarun@outlook.com>
2023-03-10 19:17:26 +0700

    complete part 2

---
+++ challenge1.js
@@ -0,0 +1,4 @@
+let myBirthYear = 1995;
+let futureYear = 2033;
+
+console.log(Math.abs(myBirthYear - futureYear));
---
+++ challenge2.js
@@ -0,0 +1,20 @@
+const jason = {
+    name: "jason",
+    location: "London",
+    hobby: "gaming everyday"
+};
+
+const tony = {
+    name: "tony",
+    location: "Japan",
+    hobby: "walking everyweek"
+};
+const romeo = {
+    name: "romeo",
+    location: "China",
+    hobby: "jumping everyminute"
+};
+
+console.log(`Hi, my name is ${jason.name}. I live in ${jason.location} and enjoy ${jason.hobby}.`);
+console.log(`Hi, my name is ${tony.name}. I live in ${tony.location} and enjoy ${tony.hobby}.`);
+console.log(`Hi, my name is ${romeo.name}. I live in ${romeo.location} and enjoy ${romeo.hobby}.`);
